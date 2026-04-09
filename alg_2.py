c03e268a40218c6c7644ea0fa5e3dfcdafe2adb6
V <sa813006@gmail.com>
2026-04-09 22:01:52 +0200

    Update alg_2.py

--- alg_2.py
+++ alg_2.py
@@ -1,36 +1,76 @@
 import random
 
-class Node():
+### KLASY ###
+
+class Node:
     def __init__(self, data):
         self.left = None
         self.right = None
         self.data = data
 
-def insert(node, value):
-    if node is None:
-        return Node(value)
-    if value == node.data:
-        return
-    elif value < node.data:
-        node.left = insert(node.left, value)
-    else:
-        if value > node.data:
-            node.right = insert(node.right, value)
-    return node
+class BST:
+    def __init__(self):
+        self.node = None
 
-def build_bst_fcfs(elements):
-    if not elements:
-        return None
-    root = Node(elements[0])
+    def insert(self, current_node, value):
+        if current_node is None:
+            return Node(value)
+        if value < current_node.data:
+            current_node.left = self.insert(current_node.left, value)
+        elif value > current_node.data:
+            current_node.right = self.insert(current_node.right, value)
+        return current_node
 
-    for i in range(1, len(elements)):
-        insert(root, elements[i])
+    def build_bst_fcfs(self, elements):
+        if not elements:
+            return
+        for value in elements:
+            self.node = self.insert(self.node, value)
 
-    return root
+    def print_inOrderTraverse(self, node):
+        if node is not None:
+            self.print_inOrderTraverse(node.left)
+            print(node.data)
+            self.print_inOrderTraverse(node.right)
 
-def print_inOrderTraverse(node):
-    if node is not None:
-        print_inOrderTraverse(node.left)
-        print(node.data)
-        print_inOrderTraverse(node.right)
+    def find_min_path(self):
+        path = []
+        current = self.node
+        if current is None:
+            return None, []
+        
+        while current:
+            path.append(current.data)
+            if current.left is None:
+                break
+            current = current.left
+        return current.data, path
+    
+    def find_max_path(self):
+        path = []
+        current = self.node
+        if current is None:
+            return None, []
+        
+        while current:
+            path.append(current.data)
+            if current.right is None:
+                break
+            current = current.right
+        return current.data, path
+    
+dane = random.sample(range(0, 100), 15)
 
+
+### PROGRAM ###
+
+tree = BST()
+tree.build_bst_fcfs(dane)
+
+minVal, minPath = tree.find_min_path()
+maxVal, maxPath = tree.find_max_path()
+
+print(f"Dane: {dane}")
+tree.print_inOrderTraverse(tree.node)
+print(f"Min: {minVal}, path: {minPath}")
+print(f"Max: {maxVal}, path: {maxPath}")
